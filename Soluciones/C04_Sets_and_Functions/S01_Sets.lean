a8e6d94e5df2453ebe6c4bb7b8fa8b330ff3df8d
Lucas Hernán Tarche <lucastarche@gmail.com>
2025-06-17 12:49:51 -0300

    4.1 Completo

---
+++ Soluciones/C04_Sets_and_Functions/S01_Sets.lean
@@ -0,0 +1,335 @@
+import Mathlib.Data.Set.Lattice
+import Mathlib.Data.Nat.Prime.Basic
+import MIL.Common
+
+section
+variable {α : Type*}
+variable (s t u : Set α)
+open Set
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  rw [subset_def, inter_def, inter_def]
+  rw [subset_def] at h
+  simp only [mem_setOf]
+  rintro x ⟨xs, xu⟩
+  exact ⟨h _ xs, xu⟩
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  simp only [subset_def, mem_inter_iff] at *
+  rintro x ⟨xs, xu⟩
+  exact ⟨h _ xs, xu⟩
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u := by
+  intro x xsu
+  exact ⟨h xsu.1, xsu.2⟩
+
+example (h : s ⊆ t) : s ∩ u ⊆ t ∩ u :=
+  fun x ⟨xs, xu⟩ ↦ ⟨h xs, xu⟩
+
+example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
+  intro x hx
+  have xs : x ∈ s := hx.1
+  have xtu : x ∈ t ∪ u := hx.2
+  rcases xtu with xt | xu
+  · left
+    show x ∈ s ∩ t
+    exact ⟨xs, xt⟩
+  · right
+    show x ∈ s ∩ u
+    exact ⟨xs, xu⟩
+
+example : s ∩ (t ∪ u) ⊆ s ∩ t ∪ s ∩ u := by
+  rintro x ⟨xs, xt | xu⟩
+  · left; exact ⟨xs, xt⟩
+  · right; exact ⟨xs, xu⟩
+
+example : s ∩ t ∪ s ∩ u ⊆ s ∩ (t ∪ u) := by
+  rintro x (⟨xs, xt⟩ | ⟨xs, xu⟩)
+  · have xtu : x ∈ t ∪ u := by left ; exact xt
+    exact ⟨xs, xtu⟩
+  · have xtu : x ∈ t ∪ u := by right ; exact xu
+    exact ⟨xs, xtu⟩
+
+example : (s \ t) \ u ⊆ s \ (t ∪ u) := by
+  intro x xstu
+  have xs : x ∈ s := xstu.1.1
+  have xnt : x ∉ t := xstu.1.2
+  have xnu : x ∉ u := xstu.2
+  constructor
+  · exact xs
+  intro xtu
+  -- x ∈ t ∨ x ∈ u
+  rcases xtu with xt | xu
+  · show False; exact xnt xt
+  · show False; exact xnu xu
+
+example : (s \ t) \ u ⊆ s \ (t ∪ u) := by
+  rintro x ⟨⟨xs, xnt⟩, xnu⟩
+  use xs
+  rintro (xt | xu) <;> contradiction
+
+example : s \ (t ∪ u) ⊆ (s \ t) \ u := by
+  rintro x ⟨xs, xntu⟩
+  have xnt : x ∉ t := by
+    contrapose! xntu
+    left
+    exact xntu
+  have xnu : x ∉ u := by
+    contrapose! xntu
+    right
+    exact xntu
+
+  exact ⟨⟨xs, xnt⟩, xnu⟩
+
+example : s ∩ t = t ∩ s := by
+  ext x
+  simp only [mem_inter_iff]
+  constructor
+  · rintro ⟨xs, xt⟩; exact ⟨xt, xs⟩
+  · rintro ⟨xt, xs⟩; exact ⟨xs, xt⟩
+
+example : s ∩ t = t ∩ s :=
+  Set.ext fun x ↦ ⟨fun ⟨xs, xt⟩ ↦ ⟨xt, xs⟩, fun ⟨xt, xs⟩ ↦ ⟨xs, xt⟩⟩
+
+example : s ∩ t = t ∩ s := by ext x; simp [and_comm]
+
+example : s ∩ t = t ∩ s := by
+  apply Subset.antisymm
+  · rintro x ⟨xs, xt⟩; exact ⟨xt, xs⟩
+  · rintro x ⟨xt, xs⟩; exact ⟨xs, xt⟩
+
+example : s ∩ t = t ∩ s :=
+    Subset.antisymm (fun x ⟨xs, xt⟩ ↦ ⟨xt, xs⟩) (fun x ⟨xt, xs⟩ ↦ ⟨xs, xt⟩)
+
+example : s ∩ (s ∪ t) = s := by
+  ext x
+  constructor
+  · rintro ⟨xs, xst⟩
+    exact xs
+  · intro xs
+    have xst : x ∈ s ∪ t := by left ; exact xs
+    exact ⟨xs, xst⟩
+
+example : s ∪ s ∩ t = s := by
+  ext x
+  constructor
+  · intro h
+    rcases h with xs | xst
+    · exact xs
+    · exact And.left xst
+  · intro xs
+    left
+    exact xs
+
+example : s \ t ∪ t = s ∪ t := by
+  ext x
+  constructor
+  · rintro (⟨xs, _⟩ | xt)
+    · left ; exact xs
+    · right ; exact xt
+  · rintro (xs | xt)
+    · by_cases xt : x ∈ t
+      · right ; exact xt
+      · left ; exact ⟨xs, xt⟩
+    · right ; exact xt
+
+example : s \ t ∪ t \ s = (s ∪ t) \ (s ∩ t) := by
+  ext x
+  constructor
+  · rintro (⟨xs, xnt⟩ | ⟨xt, xns⟩)
+    · have xsut : x ∈ s ∪ t := by left ; exact xs
+      have xnst : x ∉ s ∩ t := by
+        contrapose! xnt
+        exact And.right xnt
+      exact ⟨xsut, xnst⟩
+    · have xsut : x ∈ s ∪ t := Or.inr xt
+      have xnst : x ∉ s ∩ t := by
+        contrapose! xns
+        exact And.left xns
+      exact ⟨xsut, xnst⟩
+  · rintro ⟨(xs | xt), xnst⟩
+    · have xnt : x ∉ t := by
+        contrapose! xnst
+        exact ⟨xs, xnst⟩
+      exact Or.inl ⟨xs, xnt⟩
+    · have xns : x ∉ s := by
+        contrapose! xnst
+        exact ⟨xnst, xt⟩
+      exact Or.inr ⟨xt, xns⟩
+
+def evens : Set ℕ :=
+  { n | Even n }
+
+def odds : Set ℕ :=
+  { n | ¬Even n }
+
+example : evens ∪ odds = univ := by
+  rw [evens, odds]
+  ext n
+  simp [-Nat.not_even_iff_odd]
+  apply Classical.em
+
+example (x : ℕ) (h : x ∈ (∅ : Set ℕ)) : False :=
+  h
+
+example (x : ℕ) : x ∈ (univ : Set ℕ) :=
+  trivial
+
+example : { n | Nat.Prime n } ∩ { n | n > 2 } ⊆ { n | ¬Even n } := by
+  intro n
+  simp
+  intro h₀ h₁
+
+  -- #check Nat.Prime.eq_two_or_odd'
+  apply Nat.Prime.eq_two_or_odd' at h₀
+  rcases h₀ with (h | h)
+  · exfalso
+    rw [h] at h₁
+    contradiction
+  · exact h
+
+#print Prime
+
+#print Nat.Prime
+
+example (n : ℕ) : Prime n ↔ Nat.Prime n :=
+  Nat.prime_iff.symm
+
+example (n : ℕ) (h : Prime n) : Nat.Prime n := by
+  rw [Nat.prime_iff]
+  exact h
+
+example (n : ℕ) (h : Prime n) : Nat.Prime n := by
+  rwa [Nat.prime_iff]
+
+end
+
+section
+
+variable (s t : Set ℕ)
+
+example (h₀ : ∀ x ∈ s, ¬Even x) (h₁ : ∀ x ∈ s, Prime x) : ∀ x ∈ s, ¬Even x ∧ Prime x := by
+  intro x xs
+  constructor
+  · apply h₀ x xs
+  apply h₁ x xs
+
+example (h : ∃ x ∈ s, ¬Even x ∧ Prime x) : ∃ x ∈ s, Prime x := by
+  rcases h with ⟨x, xs, _, prime_x⟩
+  use x, xs
+
+section
+variable (ssubt : s ⊆ t)
+
+example (h₀ : ∀ x ∈ t, ¬Even x) (h₁ : ∀ x ∈ t, Prime x) : ∀ x ∈ s, ¬Even x ∧ Prime x := by
+  intro x xs
+  have xt : x ∈ t := ssubt xs
+  constructor
+  · exact h₀ x xt
+  · exact h₁ x xt
+
+example (h : ∃ x ∈ s, ¬Even x ∧ Prime x) : ∃ x ∈ t, Prime x := by
+  rcases h with ⟨x, xs, xneven, xprime⟩
+  have xt : x ∈ t := ssubt xs
+  use x
+
+end
+
+end
+
+section
+variable {α I : Type*}
+variable (A B : I → Set α)
+variable (s : Set α)
+
+open Set
+
+example : (s ∩ ⋃ i, A i) = ⋃ i, A i ∩ s := by
+  ext x
+  simp only [mem_inter_iff, mem_iUnion]
+  constructor
+  · rintro ⟨xs, ⟨i, xAi⟩⟩
+    exact ⟨i, xAi, xs⟩
+  rintro ⟨i, xAi, xs⟩
+  exact ⟨xs, ⟨i, xAi⟩⟩
+
+example : (⋂ i, A i ∩ B i) = (⋂ i, A i) ∩ ⋂ i, B i := by
+  ext x
+  simp only [mem_inter_iff, mem_iInter]
+  constructor
+  · intro h
+    constructor
+    · intro i
+      exact (h i).1
+    intro i
+    exact (h i).2
+  rintro ⟨h1, h2⟩ i
+  constructor
+  · exact h1 i
+  exact h2 i
+
+
+example : (s ∪ ⋂ i, A i) = ⋂ i, A i ∪ s := by
+  ext x
+  simp only [mem_inter_iff, mem_iInter]
+  constructor
+  · rintro (xs | xAi) i
+    · exact Or.inr xs
+    · left
+      apply xAi
+      use i
+  · rintro h
+    by_cases xs : x ∈ s
+    · exact Or.inl xs
+    · right
+      simp only [mem_inter_iff, mem_iInter]
+      intro i
+      rcases h i with h | h
+      · exact h
+      · contradiction
+
+def primes : Set ℕ :=
+  { x | Nat.Prime x }
+
+example : (⋃ p ∈ primes, { x | p ^ 2 ∣ x }) = { x | ∃ p ∈ primes, p ^ 2 ∣ x } :=by
+  ext
+  rw [mem_iUnion₂]
+  simp
+
+example : (⋃ p ∈ primes, { x | p ^ 2 ∣ x }) = { x | ∃ p ∈ primes, p ^ 2 ∣ x } := by
+  ext
+  simp
+
+example : (⋂ p ∈ primes, { x | ¬p ∣ x }) ⊆ { x | x = 1 } := by
+  intro x
+  contrapose!
+  simp
+  apply Nat.exists_prime_and_dvd
+
+example : (⋃ p ∈ primes, { x | x ≤ p }) = univ := by
+  apply eq_univ_of_forall
+  intro x
+  obtain ⟨p, ⟨xlep, pprime⟩⟩ := Nat.exists_infinite_primes x
+
+  simp only [mem_iUnion]
+  use p, pprime, xlep
+
+end
+
+section
+
+open Set
+
+variable {α : Type*} (s : Set (Set α))
+
+example : ⋃₀ s = ⋃ t ∈ s, t := by
+  ext x
+  rw [mem_iUnion₂]
+  simp
+
+example : ⋂₀ s = ⋂ t ∈ s, t := by
+  ext x
+  rw [mem_iInter₂]
+  rfl
+
+end
